9e4bc94718c99ce425484eff51ba0d179dd4709e
Chris Cowherd <madcow@gmail.com>
2019-08-31 09:39:31 -0700

    get celery urls from environment

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,8 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Broker settings.
-broker_url = 'redis://localhost:6379/15'
+broker_url = os.getenv('CELERY_BROKER_URL')
 
 # List of modules to import when the Celery worker starts.
 imports = ('jobs',)
 
 ## Using the database to store task state and results.
-result_backend = 'redis://localhost:6379/15'
+result_backend = os.getenv('CELERY_BACKEND_URL')
