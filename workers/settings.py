362326a472073ab31adfcf9428cc2deadfe77870
Chris Cowherd <chris@Chriss-iMac.local>
2019-10-11 10:49:27 -0700

    restore queue db

--- workers/settings.py
+++ workers/settings.py
@@ -6,7 +6,7 @@ REDIS_HOST = os.getenv('REDIS_HOST')
 REDIS_PORT = int(os.getenv('REDIS_PORT'))
 
 # You can also specify the Redis DB to use
-# REDIS_DB = os.getenv('WORK_QUEUE_DB')
+REDIS_DB = os.getenv('WORK_QUEUE_DB')
 # REDIS_PASSWORD = 'very secret'
 
 # Queues to listen on
